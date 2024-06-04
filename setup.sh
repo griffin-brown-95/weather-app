e8e0977350ba76b5c7874a8b561a0f6bb20d6d4a
griffin-brown-95 <154379139+griffin-brown-95@users.noreply.github.com>
2024-06-04 15:02:09 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,12 +2,12 @@ mkdir -p ~/.streamlit/
 
 echo "\
 [general]\n\
-email = \"\"\n\
+email = \"griffinb95@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
 
 echo "\
 [server]\n\
 headless = true\n\
-enableCORS = false\n\
+enableCORS=false\n\
 port = $PORT\n\
 " > ~/.streamlit/config.toml
